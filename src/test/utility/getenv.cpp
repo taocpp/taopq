69b754d58b32e5b99b7fa1703770435aa4057fec
Daniel Frey <d.frey@gmx.de>
2016-03-28 13:53:10 +0200

    New

---
+++ src/test/utility/getenv.cpp
@@ -0,0 +1,16 @@
+// The Art of C++ / Utility
+// Copyright (c) 2016 Daniel Frey
+
+#include "../macros.hpp"
+
+#include <tao/utility/getenv.hpp>
+
+int main()
+{
+  TEST_ASSERT( !tao::utility::getenv( "USER" ).empty() );
+  TEST_THROWS( tao::utility::getenv( "TAO_DOESNOTEXIST" ) );
+
+  TEST_ASSERT( !tao::utility::getenv( "USER", "" ).empty() );
+  TEST_ASSERT( tao::utility::getenv( "TAO_DOESNOTEXIST", "" ).empty() );
+  TEST_ASSERT( tao::utility::getenv( "TAO_DOESNOTEXIST", "DEFAULT VALUE" ) == "DEFAULT VALUE" );
+}
