1984570dfcb9a04c85a38708dde688077a8742f9
Daniel Frey <d.frey@gmx.de>
2016-11-25 16:07:45 +0100

    Preliminary Windows support

--- include/tao/utility/printf.hpp
+++ include/tao/utility/printf.hpp
@@ -10,8 +10,13 @@ namespace tao
 {
   namespace utility
   {
+#ifdef WIN32
+    std::string printf( const char* format, ... );
+    std::string vprintf( const char* format, va_list ap );
+#else
     std::string printf( const char* format, ... ) __attribute__(( format( printf, 1, 2 ) ));
     std::string vprintf( const char* format, va_list ap ) __attribute__(( format( printf, 1, 0 ) ));
+#endif
   }
 }
 
--- src/lib/utility/demangle.cpp
+++ src/lib/utility/demangle.cpp
@@ -3,9 +3,11 @@
 
 #include <tao/utility/demangle.hpp>
 
+#ifndef WIN32
 #include <memory>
 #include <cstdlib>
 #include <cxxabi.h>
+#endif
 
 namespace tao
 {
@@ -13,8 +15,12 @@ namespace tao
   {
     std::string demangle( const char* const symbol )
     {
+#ifdef WIN32
+      return symbol;
+#else
       const std::unique_ptr< char, decltype( &std::free ) > demangled( abi::__cxa_demangle( symbol, nullptr, nullptr, nullptr ), &std::free );
       return demangled ? demangled.get() : symbol;
+#endif
     }
 
     std::string demangle( const std::type_info& type_info )
