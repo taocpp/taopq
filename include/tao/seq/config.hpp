480125224b3117377c0cd63a9eb3fe1bf1897c0e
Daniel Frey <d.frey@gmx.de>
2018-01-25 23:28:56 +0100

    Update detection of integer_sequence availability

--- include/tao/seq/config.hpp
+++ include/tao/seq/config.hpp
@@ -1,22 +1,26 @@
-// Copyright (c) 2015-2017 Daniel Frey
+// Copyright (c) 2015-2018 Daniel Frey
 // Please see LICENSE for license or visit https://github.com/taocpp/sequences/
 
 #ifndef TAOCPP_SEQUENCES_INCLUDE_CONFIG_HPP
 #define TAOCPP_SEQUENCES_INCLUDE_CONFIG_HPP
 
-#if __cplusplus >= 201402L
+#include <utility>
 
+#ifndef TAOCPP_USE_STD_INTEGER_SEQUENCE
+#if defined( __cpp_lib_integer_sequence ) || ( defined( _LIBCPP_VERSION ) && ( __cplusplus >= 201402L ) )
 #define TAOCPP_USE_STD_INTEGER_SEQUENCE
-
-#if defined( _LIBCPP_VERSION )
-#define TAOCPP_USE_STD_MAKE_INTEGER_SEQUENCE
-#elif defined( _GLIBCXX_RELEASE ) && ( _GLIBCXX_RELEASE >= 8 )
-#define TAOCPP_USE_STD_MAKE_INTEGER_SEQUENCE
-#elif defined( _MSC_VER ) && ( _MSC_VER >= 190023918 )
-#define TAOCPP_USE_STD_MAKE_INTEGER_SEQUENCE
+#endif
 #endif
 
-#endif  // __cplusplus >= 201402L
+#ifndef TAOCPP_USE_STD_MAKE_INTEGER_SEQUENCE
+#if defined( _LIBCPP_VERSION ) && ( __cplusplus >= 201402L )
+#define TAOCPP_USE_STD_MAKE_INTEGER_SEQUENCE
+#elif defined( _GLIBCXX_RELEASE ) && ( _GLIBCXX_RELEASE >= 8 ) && ( __cplusplus >= 201402L )
+#define TAOCPP_USE_STD_MAKE_INTEGER_SEQUENCE
+#elif defined( _MSC_VER ) && ( _MSC_FULL_VER >= 190023918 )
+#define TAOCPP_USE_STD_MAKE_INTEGER_SEQUENCE
+#endif
+#endif
 
 #if defined( __cpp_fold_expressions )
 #define TAOCPP_FOLD_EXPRESSIONS
