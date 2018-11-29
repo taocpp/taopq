dd2257d7a983903ea20d27af4e36a41a91bd85b7
Uilian Ries <uilianries@gmail.com>
2018-11-29 12:47:22 -0200

    #12 Build Conan package on Travis CI
    - Add support for Linux and Macos
    - Add build scripts for Travis
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from conans import ConanFile, CMake, tools
+from conans.model.version import Version
+from conans.errors import ConanInvalidConfiguration
 
-class PostgreSQLConan(ConanFile):
+class TaopqConan(ConanFile):
     name = "taopq"
     description = "C++ client library for PostgreSQL"
     homepage = "https://github.com/taocpp/taopq"
@@ -11,7 +13,7 @@ class PostgreSQLConan(ConanFile):
     author = "taocpp@icemx.net"
     settings = "os", "build_type", "compiler", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
+    default_options = {"shared": False, "fPIC": True}
     exports = "LICENSE"
     exports_sources = "include/*", "src/*", "CMakeLists.txt", "cmake/*"
     generators = "cmake"
@@ -21,7 +23,15 @@ class PostgreSQLConan(ConanFile):
         if self.settings.os == "Windows":
             self.options.remove("fPIC")
 
-    def configure_cmake(self):
+    def configure(self):
+        compiler_version = Version(self.settings.compiler.version.value)
+        if (self.settings.compiler == "gcc" and compiler_version < "7") or \
+           (self.settings.compiler == "clang" and compiler_version < "5") or \
+           (self.settings.compiler == "apple-clang" and compiler_version < "10") or \
+           (self.settings.compiler == "Visual Studio" and compiler_version < "15"):
+            raise ConanInvalidConfiguration("taocpp/taopq requires C++17 support")
+
+    def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["TAOPQ_BUILD_TESTS"] = False
         cmake.definitions["TAOPQ_INSTALL_DOC_DIR"] = "licenses"
@@ -29,11 +39,11 @@ class PostgreSQLConan(ConanFile):
         return cmake
 
     def build(self):
-        cmake = self.configure_cmake()
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        cmake = self.configure_cmake()
+        cmake = self._configure_cmake()
         cmake.install()
 
     def package_info(self):
