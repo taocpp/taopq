e056f21a66d463b358510cdabeeab3f006a63858
Daniel Frey <d.frey@gmx.de>
2020-01-25 18:26:57 +0100

    New

---
+++ src/test/pq/traits.cpp
@@ -0,0 +1,61 @@
+// Copyright (c) 2019 Daniel Frey and Dr. Colin Hirsch
+// Please see LICENSE for license or visit https://github.com/taocpp/taopq/
+
+#include "../getenv.hpp"
+#include "../macros.hpp"
+
+#include <tao/pq/connection.hpp>
+#include <tao/pq/result_traits_tuple.hpp>
+
+void run()
+{
+   const auto connection = tao::pq::connection::create( tao::pq::internal::getenv( "TAOPQ_TEST_DATABASE", "dbname=template1" ) );
+
+   connection->execute( "DROP TABLE IF EXISTS tao_parameter_test" );
+   connection->execute( "CREATE TABLE tao_parameter_test ( a INTEGER PRIMARY KEY, b INTEGER, c INTEGER, d INTEGER )" );
+
+   TEST_EXECUTE( connection->execute( "INSERT INTO tao_parameter_test VALUES ( 1, 2, 3, 4 )" ) );
+   TEST_EXECUTE( connection->execute( "INSERT INTO tao_parameter_test VALUES ( $1, $2, $3, $4 )", 2, 3, 4, 5 ) );
+   TEST_EXECUTE( connection->execute( "INSERT INTO tao_parameter_test VALUES ( $1, $2, $3, $4 )", 3, std::make_tuple( 4, 5 ), 6 ) );
+   TEST_EXECUTE( connection->execute( "INSERT INTO tao_parameter_test VALUES ( $1, $2, $3, $4 )", std::make_tuple( 4, 5 ), std::make_tuple( 6, 7 ) ) );
+   TEST_EXECUTE( connection->execute( "INSERT INTO tao_parameter_test VALUES ( $1, $2, $3, $4 )", std::make_tuple( 5, std::make_tuple( 6, 7 ), 8 ) ) );
+
+   const auto result = connection->execute( "SELECT * FROM tao_parameter_test" );
+   TEST_ASSERT( result.size() == 5 );
+
+   for( const auto& row : result ) {
+      {
+         const auto v = row.tuple< int, int, int, int >();
+         TEST_ASSERT( std::get< 1 >( v ) == std::get< 0 >( v ) + 1 );
+         TEST_ASSERT( std::get< 2 >( v ) == std::get< 1 >( v ) + 1 );
+         TEST_ASSERT( std::get< 3 >( v ) == std::get< 2 >( v ) + 1 );
+      }
+      {
+         const auto v = row.tuple< std::tuple< int, int >, std::tuple< int, int > >();
+         TEST_ASSERT( std::get< 1 >( std::get< 0 >( v ) ) == std::get< 0 >( std::get< 0 >( v ) ) + 1 );
+         TEST_ASSERT( std::get< 0 >( std::get< 1 >( v ) ) == std::get< 1 >( std::get< 0 >( v ) ) + 1 );
+         TEST_ASSERT( std::get< 1 >( std::get< 1 >( v ) ) == std::get< 0 >( std::get< 1 >( v ) ) + 1 );
+      }
+      {
+         const auto v = row.tuple< int, std::tuple< int, int >, int >();
+         TEST_ASSERT( std::get< 0 >( std::get< 1 >( v ) ) == std::get< 0 >( v ) + 1 );
+         TEST_ASSERT( std::get< 1 >( std::get< 1 >( v ) ) == std::get< 0 >( std::get< 1 >( v ) ) + 1 );
+         TEST_ASSERT( std::get< 2 >( v ) == std::get< 1 >( std::get< 1 >( v ) ) + 1 );
+      }
+   }
+}
+
+int main()
+{
+   try {
+      run();
+   }
+   catch( const std::exception& e ) {
+      std::cerr << "exception: " << e.what() << std::endl;
+      throw;
+   }
+   catch( ... ) {
+      std::cerr << "unknown exception" << std::endl;
+      throw;
+   }
+}
