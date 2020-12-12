f2067d5528d4ddc9d07dfb7f679405396ebb2505
Daniel Frey <d.frey@gmx.de>
2020-12-12 23:20:59 +0100

    Use PATH instead of USER/WINDIR

--- src/test/pq/getenv.cpp
+++ src/test/pq/getenv.cpp
@@ -6,18 +6,10 @@
 
 void run()
 {
-#ifdef WIN32
-   TEST_ASSERT( !tao::pq::internal::getenv( "WINDIR" ).empty() );
-#else
-   TEST_ASSERT( !tao::pq::internal::getenv( "USER" ).empty() );
-#endif
+   TEST_ASSERT( !tao::pq::internal::getenv( "PATH" ).empty() );
    TEST_THROWS( tao::pq::internal::getenv( "TAOPQ_DOESNOTEXIST" ) );
 
-#ifdef WIN32
-   TEST_ASSERT( !tao::pq::internal::getenv( "WINDIR", "" ).empty() );
-#else
-   TEST_ASSERT( !tao::pq::internal::getenv( "USER", "" ).empty() );
-#endif
+   TEST_ASSERT( !tao::pq::internal::getenv( "PATH", "" ).empty() );
    TEST_ASSERT( tao::pq::internal::getenv( "TAOPQ_DOESNOTEXIST", "" ).empty() );
    TEST_ASSERT( tao::pq::internal::getenv( "TAOPQ_DOESNOTEXIST", "DEFAULT VALUE" ) == "DEFAULT VALUE" );
 }
