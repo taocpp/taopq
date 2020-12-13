f50cd35af0a1124a95d1ec4e3ee051971da7b64d
Daniel Frey <d.frey@gmx.de>
2020-12-13 01:49:40 +0100

    New

---
+++ include/tao/pq/internal/dependent_false.hpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 Daniel Frey and Dr. Colin Hirsch
+// Please see LICENSE for license or visit https://github.com/taocpp/taopq/
+
+#ifndef TAO_PQ_INTERNAL_DEPENDENT_FALSE_HPP
+#define TAO_PQ_INTERNAL_DEPENDENT_FALSE_HPP
+
+namespace tao::pq::internal
+{
+   template< typename... >
+   inline constexpr bool dependent_false = false;
+
+}  // namespace tao::pq::internal
+
+#endif
---
+++ include/tao/pq/internal/unreachable.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2020 Daniel Frey and Dr. Colin Hirsch
+// Please see LICENSE for license or visit https://github.com/taocpp/taopq/
+
+#ifndef TAO_PQ_INTERNAL_UNREACHABLE_HPP
+#define TAO_PQ_INTERNAL_UNREACHABLE_HPP
+
+#if defined( _MSC_VER )
+#define TAO_PQ_UNREACHABLE __assume( false )
+#else
+#define TAO_PQ_UNREACHABLE __builtin_unreachable()
+#endif
+
+#endif
