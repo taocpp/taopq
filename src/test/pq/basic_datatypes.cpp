43e8365fb59c425004ea5d9cbf48467fcdbb310c
Daniel Frey <d.frey@gmx.de>
2021-07-25 00:11:16 +0200

    Fix test

--- src/test/pq/basic_datatypes.cpp
+++ src/test/pq/basic_datatypes.cpp
@@ -262,11 +262,11 @@ void run()
    check< double >( "NUMERIC", NAN );
 
    check< long double >( "NUMERIC", std::numeric_limits< long double >::lowest() );
-   if constexpr( std::numeric_limits< long double >::max() > std::numeric_limits< double >::max() ) {
-      check< long double >( "NUMERIC", -1e1001L );
-      check< long double >( "NUMERIC", -1e1000L );
-      check< long double >( "NUMERIC", -1e999L );
-   }
+#if defined( __i386__ )
+   check< long double >( "NUMERIC", -1e1001L );
+   check< long double >( "NUMERIC", -1e1000L );
+   check< long double >( "NUMERIC", -1e999L );
+#endif
    check< long double >( "NUMERIC", -1e308 );
    check< long double >( "NUMERIC", -1.25 );
    check< long double >( "NUMERIC", -1 );
@@ -288,11 +288,11 @@ void run()
    check< long double >( "NUMERIC", 1 + std::numeric_limits< long double >::epsilon() );
    check< long double >( "NUMERIC", 1.25 );
    check< long double >( "NUMERIC", 1e308 );
-   if constexpr( std::numeric_limits< long double >::max() > std::numeric_limits< double >::max() ) {
-      check< long double >( "NUMERIC", 1e999L );
-      check< long double >( "NUMERIC", 1e1000L );
-      check< long double >( "NUMERIC", 1e1001L );
-   }
+#if defined( __i386__ )
+   check< long double >( "NUMERIC", 1e999L );
+   check< long double >( "NUMERIC", 1e1000L );
+   check< long double >( "NUMERIC", 1e1001L );
+#endif
    check< long double >( "NUMERIC", std::numeric_limits< long double >::max() );
    check< long double >( "NUMERIC", NAN );
 
