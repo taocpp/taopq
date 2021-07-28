9e72cdc7f4acd7d1934ffaccf3c1daef6c10899a
Daniel Frey <d.frey@gmx.de>
2021-07-28 22:35:58 +0200

    Simplify

--- src/lib/pq/table_reader.cpp
+++ src/lib/pq/table_reader.cpp
@@ -16,35 +16,29 @@ namespace tao::pq
 {
    namespace internal
    {
-      class table_reader_transaction final
+      class transaction_guard final
          : public subtransaction_base< parameter_text_traits >  // note: the traits are never used
       {
       public:
-         explicit table_reader_transaction( const std::shared_ptr< connection >& connection )
+         explicit transaction_guard( const std::shared_ptr< connection >& connection )
             : subtransaction_base< parameter_text_traits >( connection )
          {}
 
-         ~table_reader_transaction() override
-         {}
-
-         table_reader_transaction( const table_reader_transaction& ) = delete;
-         table_reader_transaction( table_reader_transaction&& ) = delete;
-         void operator=( const table_reader_transaction& ) = delete;
-         void operator=( table_reader_transaction&& ) = delete;
+         // transaction_guard( const transaction_guard& ) = delete;
+         // transaction_guard( transaction_guard&& ) = delete;
+         // void operator=( const transaction_guard& ) = delete;
+         // void operator=( transaction_guard&& ) = delete;
 
       private:
-         void v_commit() override
-         {}
-
-         void v_rollback() override
-         {}
+         void v_commit() override {}
+         void v_rollback() override {}
       };
 
    }  // namespace internal
 
    table_reader::table_reader( const std::shared_ptr< internal::transaction >& transaction, const std::string& statement )
       : m_previous( transaction ),
-        m_transaction( std::make_shared< internal::table_reader_transaction >( transaction->m_connection ) ),
+        m_transaction( std::make_shared< internal::transaction_guard >( transaction->m_connection ) ),
         m_buffer( nullptr, &PQfreemem )
    {
       result( PQexecParams( m_transaction->underlying_raw_ptr(), statement.c_str(), 0, nullptr, nullptr, nullptr, nullptr, 0 ), result::mode_t::expect_copy_out );
@@ -78,17 +72,5 @@ namespace tao::pq
    {
       return static_cast< const char* >( m_buffer.get() );
    }
-
-   // auto table_reader::commit() -> std::size_t
-   // {
-   //    const int r = PQputCopyEnd( m_transaction->underlying_raw_ptr(), nullptr );
-   //    if( r != 1 ) {
-   //       throw std::runtime_error( "PQputCopyEnd() failed: " + m_transaction->m_connection->error_message() );
-   //    }
-   //    const auto rows_affected = result( PQgetResult( m_transaction->underlying_raw_ptr() ) ).rows_affected();
-   //    m_transaction.reset();
-   //    m_previous.reset();
-   //    return rows_affected;
-   // }
 
 }  // namespace tao::pq
