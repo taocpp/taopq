3b9a6d4c0cb1687af477d030cc86b12bd8df7401
Daniel Frey <d.frey@gmx.de>
2021-07-30 07:34:34 +0200

    clang-tidy

--- src/test/pq/basic_datatypes.cpp
+++ src/test/pq/basic_datatypes.cpp
@@ -19,27 +19,27 @@ namespace tao::pq
    using binary_view = std::basic_string_view< std::byte >;
 
    template< typename T >
-   binary_view to_binary_view( const T* data, const std::size_t size )
+   auto to_binary_view( const T* data, const std::size_t size ) -> binary_view
    {
       static_assert( sizeof( T ) == 1 );
       return { reinterpret_cast< const std::byte* >( data ), size };
    }
 
    template< typename T >
-   binary_view to_binary_view( const T& data )
+   auto to_binary_view( const T& data ) -> binary_view
    {
       return to_binary_view( std::data( data ), std::size( data ) );
    }
 
    template< typename T >
-   binary to_binary( const T* data, const std::size_t size )
+   auto to_binary( const T* data, const std::size_t size ) -> binary
    {
       static_assert( sizeof( T ) == 1 );
       return { reinterpret_cast< const std::byte* >( data ), size };
    }
 
    template< typename T >
-   binary to_binary( const T& data )
+   auto to_binary( const T& data ) -> binary
    {
       return to_binary( std::data( data ), std::size( data ) );
    }
