cc02ae1fe5f450681349b52f4b988cf8aac4270e
Daniel Frey <d.frey@gmx.de>
2021-08-11 19:36:52 +0200

    Naming

--- src/lib/pq/internal/resize_uninitialized.cpp
+++ src/lib/pq/internal/resize_uninitialized.cpp
@@ -7,32 +7,33 @@ namespace tao::pq::internal
 {
    namespace
    {
-      template< typename Tag >
-      struct result
+      template< typename T >
+      struct proxy
       {
-         using type = typename Tag::type;
-         static type ptr;
+         static typename T::type value;
       };
 
-      template< typename Tag >
-      typename result< Tag >::type result< Tag >::ptr;
+      template< typename T >
+      typename T::type proxy< T >::value;
+
+      template< typename T >
+      typename T::type proxy_v = proxy< T >::value;
 
       template< typename T, typename T::type P >
-      struct steal : result< T >
+      struct set_proxy
       {
-         /* fill it ... */
-         struct filler
+         struct setter
          {
-            filler()
+            setter()
             {
-               result< T >::ptr = P;
+               proxy< T >::value = P;
             }
          };
-         static filler filler_obj;
+         static setter instance;
       };
 
       template< typename T, typename T::type P >
-      typename steal< T, P >::filler steal< T, P >::filler_obj;
+      typename set_proxy< T, P >::setter set_proxy< T, P >::instance;
 
       struct string_set_size
       {
@@ -46,32 +47,33 @@ namespace tao::pq::internal
 
 #if defined( _LIBCPP_STRING )
 
-      template class steal< string_set_size, &std::string::__set_size >;
-      template class steal< binary_set_size, &std::basic_string< std::byte >::__set_size >;
+      template class set_proxy< string_set_size, &std::string::__set_size >;
+      template class set_proxy< binary_set_size, &std::basic_string< std::byte >::__set_size >;
 
-#elif defined( _GLIBCXX_STRING ) && _GLIBCXX_USE_CXX11_ABI
+#elif defined( _GLIBCXX_STRING )
 
-      template class steal< string_set_size, &std::string::_M_set_length >;
-      template class steal< binary_set_size, &std::basic_string< std::byte >::_M_set_length >;
+      template class set_proxy< string_set_size, &std::string::_M_set_length >;
+      template class set_proxy< binary_set_size, &std::basic_string< std::byte >::_M_set_length >;
 
 #elif defined( _MSC_VER )
 
-      template class steal< string_set_size, &std::string::_Eos >;
-      template class steal< binary_set_size, &std::basic_string< std::byte >::_Eos >;
+      template class set_proxy< string_set_size, &std::string::_Eos >;
+      template class set_proxy< binary_set_size, &std::basic_string< std::byte >::_Eos >;
 
 #else
 #warning "No implementation for resize_uninitialized available."
 #endif
+
    }  // namespace
 
    void resize_uninitialized_impl( std::string& v, const std::size_t n )
    {
-      ( v.*result< string_set_size >::ptr )( n );
+      (v.*proxy_v< string_set_size >)( n );
    }
 
    void resize_uninitialized_impl( std::basic_string< std::byte >& v, const std::size_t n )
    {
-      ( v.*result< binary_set_size >::ptr )( n );
+      (v.*proxy_v< binary_set_size >)( n );
    }
 
 }  // namespace tao::pq::internal
