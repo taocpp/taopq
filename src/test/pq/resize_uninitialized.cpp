85d64b76b9708a7bb83230e5f6ede082e4d9f068
Daniel Frey <d.frey@gmx.de>
2021-08-12 20:32:42 +0200

    New

---
+++ src/test/pq/resize_uninitialized.cpp
@@ -0,0 +1,50 @@
+// Copyright (c) 2021 Daniel Frey and Dr. Colin Hirsch
+// Please see LICENSE for license or visit https://github.com/taocpp/taopq/
+
+#include "../macros.hpp"
+
+#include <tao/pq/internal/resize_uninitialized.hpp>
+
+void test( std::string& s, const std::size_t size )
+{
+   tao::pq::internal::resize_uninitialized( s, size );
+   TEST_ASSERT( s.size() == size );
+   TEST_ASSERT( s.data()[ size ] == '\0' );
+}
+
+void run()
+{
+   std::string s = "hello";
+   test( s, 2 );
+   TEST_ASSERT( s[ 0 ] == 'h' );
+   TEST_ASSERT( s[ 1 ] == 'e' );
+
+   test( s, 5 );
+   TEST_ASSERT( s[ 0 ] == 'h' );
+   TEST_ASSERT( s[ 1 ] == 'e' );
+
+   test( s, 32 );
+   TEST_ASSERT( s[ 0 ] == 'h' );
+   TEST_ASSERT( s[ 1 ] == 'e' );
+
+   test( s, 1000000000 );
+   TEST_ASSERT( s[ 0 ] == 'h' );
+   TEST_ASSERT( s[ 1 ] == 'e' );
+}
+
+auto main() -> int
+{
+   try {
+      run();
+   }
+   // LCOV_EXCL_START
+   catch( const std::exception& e ) {
+      std::cerr << "exception: " << e.what() << std::endl;
+      throw;
+   }
+   catch( ... ) {
+      std::cerr << "unknown exception" << std::endl;
+      throw;
+   }
+   // LCOV_EXCL_STOP
+}
