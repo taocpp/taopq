f07e3f2e5a1acb18065bb04d98aea117cf333700
Daniel Frey <d.frey@gmx.de>
2021-08-13 14:11:48 +0200

    Improve coverage

--- src/test/pq/basic_datatypes.cpp
+++ src/test/pq/basic_datatypes.cpp
@@ -99,7 +99,7 @@ void check_bytea( T&& t )
 {
    TEST_ASSERT( my_connection->execute< Traits >( "UPDATE tao_basic_datatypes_test SET a=$1", std::forward< T >( t ) ).rows_affected() == 1 );
 
-   const auto result = my_connection->execute( "SELECT * FROM tao_basic_datatypes_test" )[ 0 ][ 0 ].as< std::basic_string< std::byte > >();
+   const auto result = my_connection->execute( "SELECT * FROM tao_basic_datatypes_test" )[ 0 ][ 0 ].as< std::basic_string< typename T::value_type > >();
    TEST_ASSERT( result.size() == 7 );
    TEST_ASSERT( result[ 0 ] == t[ 0 ] );
    TEST_ASSERT( result[ 1 ] == t[ 1 ] );
@@ -385,15 +385,10 @@ void run()
 
    check_null( "BYTEA" );
 
-   const std::byte bdata[] = {
-      std::byte( 'v' ),
-      std::byte( 255 ),
-      std::byte( 0 ),
-      std::byte( 'a' ),
-      std::byte( 1 ),
-      std::byte( 'b' ),
-      std::byte( 0 )
-   };
+   const unsigned char bdata[] = { 'v', 255, 0, 'a', 1, 'b', 0 };
+
+   check_bytea( std::basic_string< unsigned char >( bdata, 7 ) );
+   check_bytea( std::basic_string_view< unsigned char >( bdata, 7 ) );
 
    check_bytea( tao::pq::to_binary( bdata ) );
    check_bytea( tao::pq::to_binary_view( bdata ) );
