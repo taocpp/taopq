4020b12af4bdd3b67ed49a9a2feea4c3baf51ca1
Daniel Frey <d.frey@gmx.de>
2021-08-17 18:45:29 +0200

    More tests

--- src/test/pq/array.cpp
+++ src/test/pq/array.cpp
@@ -17,13 +17,13 @@ void run()
       connection->execute( "CREATE TABLE tao_array_test ( a INTEGER[] )" );
 
       std::array< int, 4 > v{ 1, 0, 2, 4 };
-      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 ) ", v );
+      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 )", v );
 
       std::vector< int > v2 = { 42, 1701 };
-      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 ) ", v2 );
+      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 )", v2 );
 
       v2.clear();
-      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 ) ", v2 );
+      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 )", v2 );
 
       const auto r = connection->execute( "SELECT * FROM tao_array_test" ).vector< std::vector< int > >();
       TEST_ASSERT( r.size() == 3 );
@@ -43,7 +43,7 @@ void run()
       connection->execute( "CREATE TABLE tao_array_test ( a TEXT[] )" );
 
       std::vector< std::optional< std::string > > v = { "FOO", "", "{BAR\\BAZ\"B,L;A}", "NULL", std::nullopt };
-      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 ) ", v );
+      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 )", v );
 
       const auto r = connection->execute( "SELECT * FROM tao_array_test" ).as< std::vector< std::optional< std::string > > >();
       TEST_ASSERT( r.size() == 5 );
@@ -59,7 +59,7 @@ void run()
       connection->execute( "CREATE TABLE tao_array_test ( a TEXT[][] NOT NULL )" );
 
       std::vector< std::vector< std::string > > v = { { "1", "F\"O\\O", "NULL" }, { "4", " XYZ ", "6" } };
-      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 ) ", v );
+      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 )", v );
 
       const auto r = connection->execute( "SELECT * FROM tao_array_test" ).as< std::vector< std::vector< std::string > > >();
       TEST_ASSERT( r == v );
@@ -71,6 +71,24 @@ void run()
    TEST_THROWS( connection->execute( "SELECT $1", "{NULL}" ).as< std::vector< std::string > >() );
    TEST_THROWS( connection->execute( "SELECT $1", "{\"FOO}" ).as< std::vector< std::string > >() );
    TEST_THROWS( connection->execute( "SELECT $1", "{FOO}BAR" ).as< std::vector< std::string > >() );
+
+   {
+      connection->execute( "DROP TABLE IF EXISTS tao_array_test" );
+      connection->execute( "CREATE TABLE tao_array_test ( a INTEGER NOT NULL )" );
+
+      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 )", 1 );
+      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 )", 2 );
+      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 )", 3 );
+      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 )", 4 );
+      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 )", 5 );
+      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 )", 6 );
+      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 )", 7 );
+      connection->execute( "INSERT INTO tao_array_test VALUES ( $1 )", 8 );
+
+      const std::vector< int > v = { 2, 3, 5, 6, 9 };
+      const auto r = connection->execute( "SELECT * FROM tao_array_test WHERE a = ANY( $1 )", v ).vector< int >();
+      TEST_ASSERT( r == std::vector< int >{ 2, 3, 5, 6 } );
+   }
 }
 
 auto main() -> int  // NOLINT(bugprone-exception-escape)
