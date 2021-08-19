0c547311eccad28fd1b501f3afa2dc9c48a44aeb
Daniel Frey <d.frey@gmx.de>
2021-08-19 21:35:53 +0200

    Support _GLIBCXX_USE_CXX11_ABI=0, simplify

--- src/lib/pq/internal/resize_uninitialized.cpp
+++ src/lib/pq/internal/resize_uninitialized.cpp
@@ -7,59 +7,82 @@ namespace tao::pq::internal
 {
    namespace
    {
-      template< typename T >
-      struct proxy
-      {
-         static typename T::type value;
-      };
-
-      template< typename T >
-      typename T::type proxy< T >::value;
-
-      template< typename T, typename T::type P >
-      struct set_proxy
-      {
-         struct setter
-         {
-            setter()
-            {
-               proxy< T >::value = P;
-            }
-         };
-
-         static setter instance;
-      };
-
-      template< typename T, typename T::type P >
-      typename set_proxy< T, P >::setter set_proxy< T, P >::instance;
-
-      template< typename F >
-      struct wrap
-      {
-         using type = F;
-      };
-
-      using string_set_size = wrap< void ( std::string::* )( std::size_t ) >;
-      using unsigned_set_size = wrap< void ( std::basic_string< unsigned char >::* )( std::size_t ) >;
-      using binary_set_size = wrap< void ( std::basic_string< std::byte >::* )( std::size_t ) >;
+      void resize_uninitialized_proxy( std::string& v, const std::size_t n );
+      void resize_uninitialized_proxy( std::basic_string< unsigned char >& v, const std::size_t n );
+      void resize_uninitialized_proxy( std::basic_string< std::byte >& v, const std::size_t n );
 
 #if defined( _LIBCPP_STRING )
 
-      template class set_proxy< string_set_size, &std::string::__set_size >;
-      template class set_proxy< unsigned_set_size, &std::basic_string< unsigned char >::__set_size >;
-      template class set_proxy< binary_set_size, &std::basic_string< std::byte >::__set_size >;
+      template< typename T, void ( T::*F )( std::size_t ) >
+      struct proxy
+      {
+         friend void resize_uninitialized_proxy( T& v, const std::size_t n )
+         {
+            ( v.*F )( n );
+            v[ v.size() ] = '\0';
+         }
+      };
+
+      template class proxy< std::string, &std::string::__set_size >;
+      template class proxy< std::basic_string< unsigned char >, &std::basic_string< unsigned char >::__set_size >;
+      template class proxy< std::basic_string< std::byte >, &std::basic_string< std::byte >::__set_size >;
+
+#elif defined( _GLIBCXX_STRING ) && _GLIBCXX_USE_CXX11_ABI
+
+      template< typename T, void ( T::*F )( std::size_t ) >
+      struct proxy
+      {
+         friend void resize_uninitialized_proxy( T& v, const std::size_t n )
+         {
+            ( v.*F )( n );
+         }
+      };
+
+      template class proxy< std::string, &std::string::_M_set_length >;
+      template class proxy< std::basic_string< unsigned char >, &std::basic_string< unsigned char >::_M_set_length >;
+      template class proxy< std::basic_string< std::byte >, &std::basic_string< std::byte >::_M_set_length >;
 
 #elif defined( _GLIBCXX_STRING )
 
-      template class set_proxy< string_set_size, &std::string::_M_set_length >;
-      template class set_proxy< unsigned_set_size, &std::basic_string< unsigned char >::_M_set_length >;
-      template class set_proxy< binary_set_size, &std::basic_string< std::byte >::_M_set_length >;
+      template< typename T, typename R, R* ( T::*G )() const, void ( R::*F )( std::size_t ) >
+      struct proxy
+      {
+         friend void resize_uninitialized_proxy( T& v, const std::size_t n )
+         {
+            auto* rep = ( v.*G )();
+            ( rep->*F )( n );
+         }
+      };
+
+      template class proxy< std::string,
+                            std::string::_Rep,
+                            &std::string::_M_rep,
+                            &std::string::_Rep::_M_set_length_and_sharable >;
+
+      template class proxy< std::basic_string< unsigned char >,
+                            std::basic_string< unsigned char >::_Rep,
+                            &std::basic_string< unsigned char >::_M_rep,
+                            &std::basic_string< unsigned char >::_Rep::_M_set_length_and_sharable >;
+
+      template class proxy< std::basic_string< std::byte >,
+                            std::basic_string< std::byte >::_Rep,
+                            &std::basic_string< std::byte >::_M_rep,
+                            &std::basic_string< std::byte >::_Rep::_M_set_length_and_sharable >;
 
 #elif defined( _MSC_VER )
 
-      template class set_proxy< string_set_size, &std::string::_Eos >;
-      template class set_proxy< unsigned_set_size, &std::basic_string< unsigned char >::_Eos >;
-      template class set_proxy< binary_set_size, &std::basic_string< std::byte >::_Eos >;
+      template< typename T, void ( T::*F )( std::size_t ) >
+      struct proxy
+      {
+         friend void resize_uninitialized_proxy( T& v, const std::size_t n )
+         {
+            ( v.*F )( n );
+         }
+      };
+
+      template class proxy< std::string, &std::string::_Eos >;
+      template class proxy< std::basic_string< unsigned char >, &std::basic_string< unsigned char >::_Eos >;
+      template class proxy< std::basic_string< std::byte >, &std::basic_string< std::byte >::_Eos >;
 
 #else
 #warning "No implementation for resize_uninitialized available."
@@ -69,17 +92,17 @@ namespace tao::pq::internal
 
    void resize_uninitialized_impl( std::string& v, const std::size_t n )
    {
-      ( v.*proxy< string_set_size >::value )( n );
+      resize_uninitialized_proxy( v, n );
    }
 
    void resize_uninitialized_impl( std::basic_string< unsigned char >& v, const std::size_t n )
    {
-      ( v.*proxy< unsigned_set_size >::value )( n );
+      resize_uninitialized_proxy( v, n );
    }
 
    void resize_uninitialized_impl( std::basic_string< std::byte >& v, const std::size_t n )
    {
-      ( v.*proxy< binary_set_size >::value )( n );
+      resize_uninitialized_proxy( v, n );
    }
 
 }  // namespace tao::pq::internal
