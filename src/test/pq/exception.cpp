422a77e891120e1a388f64f391d182f59a669224
Daniel Frey <d.frey@gmx.de>
2021-09-17 00:24:59 +0200

    New

---
+++ src/test/pq/exception.cpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2021 Daniel Frey and Dr. Colin Hirsch
+// Please see LICENSE for license or visit https://github.com/taocpp/taopq/
+
+#include "../getenv.hpp"
+#include "../macros.hpp"
+
+#include <tao/pq/connection.hpp>
+
+void run()
+{
+   // overwrite the default with an environment variable if needed
+   const auto connection_string = tao::pq::internal::getenv( "TAOPQ_TEST_DATABASE", "dbname=template1" );
+   const auto connection = tao::pq::connection::create( connection_string );
+   connection->execute( "DROP TABLE IF EXISTS tao_exception_test" );
+
+   TEST_THROWS( connection->execute( "SELECT a FROM tao_exception_test" ) );
+
+   connection->execute( "CREATE TABLE tao_exception_test ( a TEXT PRIMARY KEY, b TEXT NOT NULL )" );
+
+   TEST_THROWS( connection->execute( "SELECT c FROM tao_exception_test" ) );
+
+   TEST_THROWS( connection->execute( "FOO BAR BAZ" ) );
+   TEST_THROWS( connection->execute( "SELECT 1/0" ) );
+   TEST_THROWS( connection->execute( "SELECT * FROM tao_exception_test WHERE a = 42" ) );
+   TEST_THROWS( connection->execute( "SELECT * FROM tao_exception_test WHERE a[0] = 'FOO'" ) );
+}
+
+auto main() -> int  // NOLINT(bugprone-exception-escape)
+{
+   try {
+      run();
+   }
+   // LCOV_EXCL_START
+   catch( const std::exception& e ) {
+      std::cerr << "exception: " << e.what() << std::endl;
+      throw;
+   }
+   catch( ... ) {
+      std::cerr << "unknown exception" << std::endl;
+      throw;
+   }
+   // LCOV_EXCL_STOP
+}
