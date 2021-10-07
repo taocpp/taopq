244a385c8f35eea148f818dc6244560e682030db
Daniel Frey <d.frey@gmx.de>
2021-10-07 17:40:41 +0200

    Simplify

--- include/tao/pq/internal/gen.hpp
+++ include/tao/pq/internal/gen.hpp
@@ -33,16 +33,9 @@ namespace tao::pq::internal
    };
 
    template< std::size_t... Ns >
-   struct gen2
-   {
-      using S1 = std::make_index_sequence< ( 0 + ... + Ns ) >;
-      using S2 = std::make_index_sequence< sizeof...( Ns ) >;
-      using S3 = exclusive_scan_t< std::index_sequence< Ns... > >;
-      using type = make< S1, S2, S3 >;
-   };
-
-   template< std::size_t... Ns >
-   using gen = typename gen2< Ns... >::type;
+   using gen = make< std::make_index_sequence< ( 0 + ... + Ns ) >,
+                     std::make_index_sequence< sizeof...( Ns ) >,
+                     exclusive_scan_t< std::index_sequence< Ns... > > >;
 
 }  // namespace tao::pq::internal
 
