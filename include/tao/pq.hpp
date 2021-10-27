2501744fa6ba4af8d5d6402e0588e8aa8a227b60
Daniel Frey <d.frey@gmx.de>
2021-10-27 08:44:52 +0200

    Remove check

--- include/tao/pq.hpp
+++ include/tao/pq.hpp
@@ -5,12 +5,6 @@
 #ifndef TAO_PQ_HPP
 #define TAO_PQ_HPP
 
-#if __cplusplus < 201703L
-#if !defined( _MSC_VER )
-#error "taoPQ requires C++17 or newer"
-#endif
-#else
-
 #include <tao/pq/version.hpp>
 
 #include <tao/pq/binary.hpp>
@@ -41,5 +35,4 @@
 
 #include <tao/pq/large_object.hpp>
 
-#endif
 #endif
