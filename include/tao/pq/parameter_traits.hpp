fd363391744329a4418789e127741b947cb7da88
Daniel Frey <d.frey@gmx.de>
2023-12-17 21:55:08 +0100

    disable clang-format here, as different versions disagree on this part

--- include/tao/pq/parameter_traits.hpp
+++ include/tao/pq/parameter_traits.hpp
@@ -42,6 +42,7 @@ namespace tao::pq
             assert( result > 0 );
             assert( static_cast< std::size_t >( result ) < N );
          }
+         // clang-format off
          else if( std::isnan( v ) ) {
 #if defined( _MSC_VER )
             [[maybe_unused]] const auto result = strncpy_s( buffer, "NAN", N );
@@ -58,6 +59,7 @@ namespace tao::pq
             std::strcpy( buffer, ( v < 0 ) ? "-INF" : "INF" );  // NOLINT(clang-analyzer-security.insecureAPI.strcpy)
 #endif
          }
+         // clang-format on
       }
 
    }  // namespace internal
