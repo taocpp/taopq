06ceb63576d0379a79a84779d0ceac7c6a5f7822
Daniel Frey <d.frey@gmx.de>
2024-03-01 20:32:14 +0100

    Improve selection

--- include/tao/pq/internal/unreachable.hpp
+++ include/tao/pq/internal/unreachable.hpp
@@ -5,7 +5,7 @@
 #ifndef TAO_PQ_INTERNAL_UNREACHABLE_HPP
 #define TAO_PQ_INTERNAL_UNREACHABLE_HPP
 
-#if defined( _MSC_VER )
+#if defined( _MSC_VER ) && !defined( __clang__ )
 #define TAO_PQ_UNREACHABLE __assume( false )
 #else
 #define TAO_PQ_UNREACHABLE __builtin_unreachable()
