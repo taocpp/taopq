ded292312b098814935d6080e46ef9bd75821a1f
Daniel Frey <d.frey@gmx.de>
2024-10-27 15:13:22 +0100

    Use C++20 features

--- include/tao/pq/binary.hpp
+++ include/tao/pq/binary.hpp
@@ -14,29 +14,25 @@ namespace tao::pq
    using binary = std::vector< std::byte >;
    using binary_view = std::span< const std::byte >;
 
-   template< typename T >
-   [[nodiscard]] auto to_binary_view( const T* data, const std::size_t size ) noexcept -> binary_view
+   [[nodiscard]] auto to_binary_view( const auto* data, const std::size_t size ) noexcept -> binary_view
    {
-      static_assert( sizeof( T ) == 1 );
+      static_assert( sizeof( *data ) == 1 );
       return { reinterpret_cast< const std::byte* >( data ), size };
    }
 
-   template< typename T >
-   [[nodiscard]] auto to_binary_view( const T& value ) noexcept -> binary_view
+   [[nodiscard]] auto to_binary_view( const auto& value ) noexcept -> binary_view
    {
       return pq::to_binary_view( std::data( value ), std::size( value ) );
    }
 
-   template< typename T >
-   [[nodiscard]] auto to_binary( const T* data, const std::size_t size ) -> binary
+   [[nodiscard]] auto to_binary( const auto* data, const std::size_t size ) -> binary
    {
-      static_assert( sizeof( T ) == 1 );
+      static_assert( sizeof( *data ) == 1 );
       const auto* ptr = reinterpret_cast< const std::byte* >( data );
       return { ptr, ptr + size };
    }
 
-   template< typename T >
-   [[nodiscard]] auto to_binary( const T& value ) -> binary
+   [[nodiscard]] auto to_binary( const auto& value ) -> binary
    {
       return pq::to_binary( std::data( value ), std::size( value ) );
    }
