144daca97ddd5764fcadd0fe27da6ff16eaaf301
Uilian Ries <uilianries@gmail.com>
2025-10-25 14:40:11 +0200

    Add Conan recipe for libpq requirement
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ .github/conanfile.py
@@ -0,0 +1,21 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeDeps, CMakeToolchain, cmake_layout
+
+class TaopqRequirements(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires("libpq/[>=14 <17]", transitive_headers=True, transitive_libs=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.set_property("libpq", "cmake_file_name", "PostgreSQL")
+        deps.set_property("libpq", "cmake_target_name", "PostgreSQL::PostgreSQL")
+        deps.set_property("libpq", "cmake_additional_variables_prefixes", ["PostgreSQL",])
+        deps.generate()
