1b91bc97f3870e9548047224b4fe4513fe55de25
Uilian Ries <uilianries@gmail.com>
2025-10-25 17:34:49 +0200

    Use safe getenv
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- example/get_version/main.cpp
+++ example/get_version/main.cpp
@@ -1,12 +1,21 @@
+#include <cstdlib>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include <tao/pq.hpp>
 
-int main()
-{
-   const auto connection = tao::pq::connection::create( tao::pq::internal::getenv( "TAOPQ_DATABASE", "dbname=template1" ) );
+int main() {
+   std::size_t required = 1024;
+   std::vector<char> buffer(required);
+   const auto err = ::getenv_s(&required, buffer.data(), buffer.size(), "PGDATABASE");
+   if (err != 0) {
+       std::cerr << "Error retrieving environment variable PGDATABASE" << std::endl;
+       return EXIT_FAILURE;
+   }
+   const std::string database( buffer.data() );
+   const auto connection = tao::pq::connection::create( database );
    const auto result = connection->execute( "SELECT version()" );
    std::cout << "PostgreSQL version: " << result.as< std::string >() << std::endl;
-   return 0;
+   return EXIT_SUCCESS;
 }
