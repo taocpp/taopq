2252991ad5c465d0d82157679b89d1436854f3d6
Uilian Ries <uilianries@gmail.com>
2025-10-26 19:42:56 +0100

    Re-use from msvc
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .github/conan/conanfile.py
+++ .github/conan/conanfile.py
@@ -3,6 +3,16 @@ from conan.tools.cmake import CMakeDeps, CMakeToolchain, cmake_layout
 
 class TaopqRequirements(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
+    default_options = {
+        "libpq/*:with_openssl": False,
+        "libpq/*:with_icu": False,
+        "libpq/*:with_zlib": False,
+        "libpq/*:with_zstd": False,
+        "libpq/*:with_libxml2": False,
+        "libpq/*:with_lz4": False,
+        "libpq/*:with_xslt": False,
+        "libpq/*:with_readline": False
+    }
 
     def layout(self):
         cmake_layout(self)
